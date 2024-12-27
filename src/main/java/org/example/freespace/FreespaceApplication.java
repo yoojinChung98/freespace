167212aa384dee4bc1868d2cddb003c51fcaea92
yoojin <yoojin9199@gmail.com>
2024-12-28 00:57:35 +0900

    init

---
+++ src/main/java/org/example/freespace/FreespaceApplication.java
@@ -0,0 +1,13 @@
+package org.example.freespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreespaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreespaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/freespace/FreespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.freespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreespaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
